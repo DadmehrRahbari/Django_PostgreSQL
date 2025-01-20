a9b48f977491059c0ebc09adf3bd8901b4283f35
DadmehrRahbari <d.rahbari@gmail.com>
2025-01-20 23:10:29 +0200

    Initial commit

---
+++ settings.py
@@ -0,0 +1,17 @@
+# Postgre SQL
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'RRlWNn6PCe7CThhK6rIbFBYQ',
+        'HOST': 'postgres',
+        'PORT': '5432',
+    }
+}
+
+INSTALLED_APPS = (
+    'orm',
+)
+
+SECRET_KEY = 'SECRET KEY for this Django Project'
